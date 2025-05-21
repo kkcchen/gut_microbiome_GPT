93d9e3c6bb19991da672dd0e42df31d2fe7631bd
Kevin Chen <kkcc.chen@gmail.com>
2025-05-21 18:05:09 -0400

    remove electra trainers

--- trainers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .electra.electra_gen_trainer import GeneratorTrainer
-from .electra.electra_disc_trainer import DiscriminatorTrainer
-from .electra.electra_disc_finetune_trainer import DiscriminatorFinetuneTrainer
