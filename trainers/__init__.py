c77145c90c83e3ba33ed2dfc5ae8ae6496524806
Kevin Chen <kkcc.chen@gmail.com>
2025-06-02 00:46:44 -0400

    log

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -3,15 +3,16 @@ import logging
 import sys
 
 logger = get_logger('trainers')
+base_logger = logger.logger
 
 # check if logger has been initialized
-if not logger.hasHandlers() or len(logger.handlers) == 0:
-    logger.propagate = False
-    logger.setLevel(logging.INFO)
+if not base_logger.hasHandlers() or len(base_logger.handlers) == 0:
+    base_logger.propagate = False
+    base_logger.setLevel(logging.INFO)
     handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
+    base_logger.setLevel(logging.INFO)
     formatter = logging.Formatter(
         "%(name)s - %(levelname)s - %(message)s", datefmt="%H:%M:%S"
     )
     handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    base_logger.addHandler(handler)
